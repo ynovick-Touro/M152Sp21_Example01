095ceb1d9778126e963eefd566352b23918bdb7f
ynovick-Touro <yitzchak.novick@touro.edu>
2021-02-04 19:52:28 -0500

    Update Program.java

--- src/yn/Program.java
+++ src/yn/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int y;
+		int Z;
 		String s = MessageGenerator.getMessage(0);
 		System.out.println(s);
 		s = MessageGenerator.getMessage(1);
