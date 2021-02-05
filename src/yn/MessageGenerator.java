f8f77af8e99dc87e80306cacd8e1a05affe0c763
Yitzchak Novick <yitzchak.novick@touro.edu>
2021-02-04 19:52:23 -0500

    New version of hello

--- src/yn/MessageGenerator.java
+++ src/yn/MessageGenerator.java
@@ -3,7 +3,7 @@ package yn;
 public class MessageGenerator {
 	public static String getMessage(int x) {
 		if (x == 0)
-			return "Hello World";
+			return "Hi World";
 		else
 			return "GitHub";
 	}
