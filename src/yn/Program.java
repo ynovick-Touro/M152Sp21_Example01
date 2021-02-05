44549bd4e469e5e5dbe32e94e3ff3e5f5227c70d
ynovick-Touro <yitzchak.novick@touro.edu>
2021-02-04 19:41:28 -0500

    Added a second message

--- src/yn/Program.java
+++ src/yn/Program.java
@@ -4,9 +4,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = MessageGenerator.getMessage(1);
+		String s = MessageGenerator.getMessage(0);
 		System.out.println(s);
-		int x = 5;
+		s = MessageGenerator.getMessage(1);
+		System.out.println(s);
 	}
 
 }
